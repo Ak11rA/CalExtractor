9b54da1903a16953d25376ac0a4f805e0cbb8696
Jan Schreiber <js@loopback.org>
2024-04-21 13:33:15 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ o365_credentials = ('fxxx’, ‘yyy’)
 o365_tenant_id = ‘zzz
 o365_calendar_name='My Synced Cal'           # O365 calendar to sync to
 o365_resource='address@email.org'
+o365_lookbehind_days = 30                                                               # how many days back to sync
+o365_lookahead_days = 30                                                                # how many days in the future to sync
 
 # ical file configuration
 ics_filename='/Users/xxx/OneDrive/icsUpLoad/Cal_File.ics'
