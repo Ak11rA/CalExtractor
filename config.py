93de4a09511a344958272d3c342d8b246e94b2df
Jan Schreiber <js@loopback.org>
2024-04-21 12:16:24 +0200

    Create config.py (blank version)

---
+++ config.py
@@ -0,0 +1,10 @@
+# Configuration for CalExtractor
+
+# O365 connection credentials
+o365_credentials = ('fxxx’, ‘yyy’)
+o365_tenant_id = ‘zzz
+o365_calendar_name='My Synced Cal'           # O365 calendar to sync to
+o365_resource='address@email.org'
+
+# ical file configuration
+ics_filename='/Users/xxx/OneDrive/icsUpLoad/Cal_File.ics'
